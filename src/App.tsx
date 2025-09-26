fccf7825e8d48b8fc82af3ddc3b6e9bf3ad16849
Michael <84879204+M-Trevizo@users.noreply.github.com>
2025-09-26 00:58:17 -0700

    Start of project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+
+  return (
+    <main className="bg-gray-700 flex flex-col text-center justify-center  min-h-screen">
+      <h1 className="font-bold text-white">Stellar</h1>
+      <p className="font-bold text-white">Hello World!</p>
+    </main>
+  );
+}
+
+export default App;
