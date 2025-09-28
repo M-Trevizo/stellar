18eb4f4b4d20e9a4ff0b0ec412867b95a4b9a908
Michael <84879204+M-Trevizo@users.noreply.github.com>
2025-09-28 00:36:48 -0700

    Create custom title bar

---
+++ src/components/Titlebar.tsx
@@ -0,0 +1,113 @@
+// Template from Tauri.app
+import { getCurrentWindow } from "@tauri-apps/api/window";
+import { MouseEvent, useState } from "react";
+import TitleMenu from "./TitleMenu";
+
+export default function Titlebar() {
+    const appWindow = getCurrentWindow();
+    const [showFile, setShowFile] = useState<boolean>(false);
+    const [showEdit, setShowEdit] = useState<boolean>(false);
+
+    const handleDrag = (e: MouseEvent) => {
+        if (e.buttons === 1) {
+            e.detail === 2
+                ? appWindow.toggleMaximize()
+                : appWindow.startDragging();
+        }
+    }
+
+    const handleClick = (menu: "file" | "edit") => {
+        switch(menu) {
+            case "file": 
+                setShowEdit(false);
+                setShowFile(!showFile);
+            break;
+            case "edit": 
+                setShowFile(false);
+                setShowEdit(!showEdit);
+            break;
+            default: console.log("param string not recognized");
+        }
+    }
+    
+
+    return (
+        <div className="h-[30px] bg-gray-900 absolute min-w-screen flex flex-row justify-between">
+            <div className="flex justify-center items-center gap-0 text-white">
+                <div className="h-full relative">
+                    <button 
+                        className="px-2 h-full hover:cursor-pointer hover:bg-gray-700"
+                        onClick={() => handleClick("file")}
+                    >
+                        File
+                    </button>
+                    {showFile && <TitleMenu menu="file"/>}
+                </div>
+                <div className="h-full relative">
+                    <button 
+                        className="px-2 h-full hover:cursor-pointer hover:bg-gray-700"
+                        onClick={() => handleClick("edit")}
+                    >
+                        Edit
+                    </button>
+                    {showEdit && <TitleMenu menu="edit"/>}
+                </div>
+            </div>
+            <div className="w-full" onMouseDown={handleDrag}></div>
+            <div className="flex">
+                <button 
+                    id="titlebar-minimize" 
+                    title="minimize" 
+                    className="p-0 m-0 w-[30px] border-none flex flex-row justify-center items-center hover:bg-gray-700"
+                    onClick={() => appWindow.minimize()}
+                >
+                 {/* https://api.iconify.design/mdi:window-minimize.svg */}
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                >
+                    <path fill="white" d="M19 13H5v-2h14z" />
+                </svg>
+                </button>
+                <button 
+                    id="titlebar-maximize" 
+                    title="maximize" 
+                    className="p-0 m-0 w-[30px] border-none flex flex-row justify-center items-center hover:bg-gray-700"
+                    onClick={() => appWindow.toggleMaximize()}
+                >
+                {/* https://api.iconify.design/mdi:window-maximize.svg */}
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                >
+                    <path fill="white" d="M4 4h16v16H4zm2 4v10h12V8z" />
+                </svg>
+                </button>
+                <button 
+                    id="titlebar-close" 
+                    title="close" 
+                    className="p-0 m-0 w-[30px] border-none flex flex-row justify-center items-center hover:bg-red-500"
+                    onClick={() => appWindow.close()}
+                >
+                {/* https://api.iconify.design/mdi:close.svg */}
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                >
+                    <path
+                    fill="white"
+                    d="M13.46 12L19 17.54V19h-1.46L12 13.46L6.46 19H5v-1.46L10.54 12L5 6.46V5h1.46L12 10.54L17.54 5H19v1.46z"
+                    />
+                </svg>
+                </button>
+            </div>
+            
+        </div>
+    );
+}
