3ea6d0d10c60f6bac9346090ea86b078a905e8d0
Michael <84879204+M-Trevizo@users.noreply.github.com>
2025-09-28 14:08:44 -0700

    Add top padding

--- src/components/TextBox.tsx
+++ src/components/TextBox.tsx
@@ -7,7 +7,7 @@ export default function TextBox() {
         <textarea 
             rows={10} 
             cols={10}
-            className="text-white p-2 outline-0 resize-none border-white border-2 min-h-screen"
+            className="text-white p-2 pt-8 outline-0 resize-none border-white  min-h-screen"
             onChange={e => setContent(e.target.value)}
         >
         </textarea>
