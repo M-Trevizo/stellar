c7f112e2a0d2c60fd5d18042d3e9769df21855cc
Michael <84879204+M-Trevizo@users.noreply.github.com>
2025-09-29 12:25:44 -0700

    Lift states for save functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
+import { useState } from "react";
+import { invoke } from "@tauri-apps/api/core";
 import TextBox from "./components/TextBox";
 import Titlebar from "./components/Titlebar";
 
 function App() {
+  const [content, setContent] = useState("");
+
+  const saveAs = () => {
+    invoke("save_as", {content: content})
+  }
 
   return (
     <>
       <main className="bg-gray-800 flex flex-col">
-        <Titlebar />
-        <TextBox />
+        <Titlebar saveAs={saveAs}/>
+        <TextBox setContent={setContent}/>
       </main>
     </>
   );
