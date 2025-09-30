dcffbe3f89a56de0a0519281ff97aa203845e833
Michael <84879204+M-Trevizo@users.noreply.github.com>
2025-09-29 18:30:31 -0700

    Move commands from lib.rs to commands.rs

---
+++ src-tauri/src/commands.rs
@@ -0,0 +1,46 @@
+// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use std::fs::File;
+use std::io::Read;
+use std::io::Write;
+use std::io::BufWriter;
+use std::io::BufReader;
+use rfd::FileDialog;
+
+// TODO: Create custom errors to handle the IO Errors
+
+#[tauri::command]
+pub fn save_as(content: String) {
+    // Open File Explorer and get PathBuf
+    let path_buf = FileDialog::new().save_file();
+    // Check that path_buf is some value otherwise return
+    if path_buf.is_none() { return };
+    
+    // Unwrap Option and create a new file
+    // Then write to file and handle errors
+    let mut path = path_buf.unwrap();
+    path.set_extension("txt");
+    match File::create_new(path) {
+        Ok(file) => { 
+            let mut buf_writer = BufWriter::new(file);
+            buf_writer.write(content.as_bytes()).unwrap();
+        }
+        Err(error) => println!("Error: {}", error),
+    };
+}
+
+#[tauri::command]
+pub fn save(content: String) {
+
+}
+
+#[tauri::command]
+pub fn open(path: String) -> Result<String, String> {
+    // Open file and create buffered reader
+    let file = File::open(path).map_err(|err| err.to_string())?;
+    let mut buf_reader = BufReader::new(file);
+    // Create new string and read into it
+    let mut content = String::new();
+    buf_reader.read_to_string(&mut content).map_err(|err| err.to_string())?;
+    // Return content string
+    Ok(content)
+}
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,34 +1,10 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-use std::fs::File;
-use std::io::Write;
-use std::io::BufWriter;
-use rfd::FileDialog;
-
-#[tauri::command]
-fn save_as(content: String) {
-    // Open File Explorer and get PathBuf
-    let path_buf = FileDialog::new().save_file();
-    // Check that path_buf is some value otherwise return
-    if path_buf.is_none() { return };
-    
-    // Unwrap Option and create a new file
-    // Then write to file and handle errors
-    let mut path = path_buf.unwrap();
-    path.set_extension("txt");
-    match File::create_new(path) {
-        Ok(file) => { 
-            let mut buf_writer = BufWriter::new(file);
-            buf_writer.write(content.as_bytes()).unwrap();
-        }
-        Err(error) => println!("Error: {}", error),
-    };
-}
+mod commands;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![save_as])
+        .invoke_handler(tauri::generate_handler![commands::save_as, commands::open])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
