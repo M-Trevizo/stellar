efd89382ae22be03c4821476ab67763122d71dfe
Michael <84879204+M-Trevizo@users.noreply.github.com>
2025-10-03 15:34:41 -0700

    Refactor saveAs, add newFile implementation.

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,22 @@ function App() {
   const [content, setContent] = useState("");
   const [fileName, setFileName] = useState<string>("");
 
-  const saveAs = () => {
-    invoke("save_as", {content: content})
+  const saveAs = async () => {
+    try {
+      await invoke("save_as", {content: content})
+      const name = await invoke("get_file_name");
+      setFileName(name as string);
+    }
+    catch (err) {
+      console.error(err);
+    }
+    
   }
 
   const open = async () => {
     try {
-      await invoke("open");
+      const text = await invoke("open");
+      setContent(text as string);
       const name = await invoke("get_file_name");
       setFileName(name as string);
     }
@@ -31,10 +40,22 @@ function App() {
     }
   }
 
+  const newFile = () => {
+    invoke("new");
+    setContent("");
+    setFileName("");
+  }
+
   return (
     <>
       <main className="bg-gray-800 flex flex-col">
-        <Titlebar fileName={fileName} open={open} save={save} saveAs={saveAs}/>
+        <Titlebar 
+          fileName={fileName} 
+          open={open} 
+          save={save} 
+          saveAs={saveAs}
+          newFile={newFile}
+        />
         <TextBox content={content} setContent={setContent}/>
       </main>
     </>
